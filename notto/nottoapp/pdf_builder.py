fde1ef1c628ed1db70943758d148524507f9c83e
alexNeto <alex.neto3@hotmail.com>
2017-12-17 15:12:57 -0200

    add convertion to pdf

---
+++ notto/nottoapp/pdf_builder.py
@@ -0,0 +1,17 @@
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.template import Context
+from cgi import escape
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result, encoding='UTF-8')
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
--- notto/nottoapp/urls.py
+++ notto/nottoapp/urls.py
@@ -7,6 +7,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:note_name>.pdf', views.html2pdf),
     path('<str:note_name>', views.note, name='note'),
     re_path(r'^(?P<note_name>)', views.note, name='note')
+    
 ]
