244d4391bcb9f7c3fad38ebbfe7ec2b773853088
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-07 12:29:12 +0200

    Refactoring code style (#32)
    * Add .idea in gitignore
    
    * All usefull commands add to Makefile like rules. Update .travis.yml with this rules. Update steps in Readme for linux/osx
    
    * Refactoring code style for flake8
    
    * Update tox.ini. Add migrations and nottoenv to exclude filter.
    
    * Add all requirements in requirements.txt

--- notto/nottoapp/models.py
+++ notto/nottoapp/models.py
@@ -1,13 +1,14 @@
-'''
+"""
 Notto models
-'''
+"""
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Note(models.Model):
-    '''
+    """
     A note made by a user (logged in or not)
-    '''
+    """
     content = models.TextField()
     url_title = models.CharField(
         max_length=500,
@@ -28,12 +29,12 @@ class Note(models.Model):
     def __str__(self):
         if self.created_by is not None:
             return '{} by {}.'.format(self.url_title, self.created_by.name)
-        return '{} by annonymous.'.format(self.url_title)
+        return '{} by anonymous.'.format(self.url_title)
 
     def get_children(self):
-        '''
+        """
         Get children notes
-        '''
+        """
         children = None
         if self.url_title is not None:
             children = Note.objects.filter(
@@ -42,17 +43,17 @@ class Note(models.Model):
         return children
 
     def has_parent(self):
-        '''
+        """
         Returns if this note has a parent note
-        '''
+        """
         if len(self.url_title.split('/')) > 1:
             return True
         return False
 
     def get_parent(self):
-        '''
+        """
         Get parent note
-        '''
+        """
         parent = [None]
         if self.url_title is not None and self.has_parent():
             parent = Note.objects.filter(
--- notto/nottoapp/pdf_builder.py
+++ notto/nottoapp/pdf_builder.py
@@ -1,21 +1,23 @@
-'''
+"""
 module to generate the pdf
-'''
+"""
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 
+
 def render_to_pdf(template_src, context_dict):
-    '''
+    """
     renders a document to pdf using a template
-    '''
+    """
     if context_dict is None:
         context_dict = {}
     template = get_template(template_src)
     html = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result, encoding='UTF-8')
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result,
+                            encoding='UTF-8')
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
